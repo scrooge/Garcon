564f83c188b218f04667176a569421e09ecfe8ad
Jacob Salomón <jacsalomon@yahoo.com>
2011-07-19 14:43:54 -0500

    Se agregan filtros de validación a la forma
    Se agregan filtros de Regex, tamaño de cadena y números

--- application/forms/Productos.php
+++ application/forms/Productos.php
@@ -4,40 +4,48 @@ class Application_Form_Productos extends Zend_Form
 {
     public function init()
     {
-    	$this->setName('producto');
+        $this->setName('producto');
 
     	$id = new Zend_Form_Element_Hidden( 'id' );
     	$id->addFilter( 'Int' );
 
     	$nombre = new Zend_Form_Element_Text( 'nombre' );
     	$nombre->setLabel( 'Nombre' )
-               ->setRequired( 'true' )
-               ->addFilter( 'StripTags' )
-               ->addFilter( 'StringTrim' )
-               ->addValidator( 'NotEmpty' );
+            ->setRequired( 'true' )
+            ->addFilter( 'StripTags' )
+            ->addFilter( 'StringTrim' )
+            ->addValidator( 'NotEmpty' );
+	     ->addValidator( 'Regex', false, array('#^([a-z A-Z0-9ñÑáéíóúÁÉÍÓÚüÜ]$
+            ->addValidator( 'stringLength', true, array(1, 255));
+
 
     	$descripcion = new Zend_Form_Element_Text( 'descripcion' );
     	$descripcion->setLabel( 'Descripción' )
-    	            ->setRequired( 'true' )
-    	            ->addFilter( 'StripTags' )
-    	            ->addFilter( 'StringTrim' )
-    	            ->addValidator( 'NotEmpty' );
+    	    ->setRequired( 'true' )
+    	    ->addFilter( 'StripTags' )
+    	    ->addFilter( 'StringTrim' )
+    	    ->addValidator( 'NotEmpty' );
+	    ->addValidator( 'Regex', false, array('#^([a-zA-Z0-9ñÑáéíóúÁÉÍÓÚüÜ]$
+            ->addValidator( 'stringLength', true, array(1, 255));
+ 		    	
 
     	$precio = new Zend_Form_Element_Text( 'precio' );
     	$precio->setLabel( 'Precio' )
-    	       ->setRequired( 'true' )
-    	       ->addFilter( 'StripTags' )
-    	       ->addFilter( 'StringTrim' )
-    	       ->addValidator( 'NotEmpty' )
-    	       ->addValidator( 'Float' );
+    	     ->setRequired( 'true' )
+    	     ->addFilter( 'StripTags' )
+    	     ->addFilter( 'StringTrim' )
+    	     ->addValidator( 'NotEmpty' )
+    	     ->addValidator( 'Float' );
 
     	$existencia = new Zend_Form_Element_Text( 'existencia' );
     	$existencia->setLabel( 'Existencia' )
-    	           ->setRequired( 'true' )
-    	           ->addFilter( 'StripTags' )
-    	           ->addFilter( 'StringTrim' )
-    	           ->addValidator( 'NotEmpty' );
-    	
+    	      ->setRequired( 'true' )
+    	      ->addFilter( 'StripTags' )
+    	      ->addFilter( 'StringTrim' )
+    	      ->addFilter( 'Int' )
+	      ->addValidator( 'NotEmpty' );
+    	      
+
     	$enviar = new Zend_Form_Element_Submit( 'enviar' );
     	$enviar->setAttrib( 'id', 'botonEnviar' );
 
